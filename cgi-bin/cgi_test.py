e80bd7e27920fe4a2e247b95d97c672828b90e81
Jason Juhyun Kim <mtinet@hanmail.net>
2020-05-24 01:27:13 +0900

    Create cgi_test.py

---
+++ cgi-bin/cgi_test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import cgi
+import cgitb    #스크립트 오류 등의 에러발생시 브라우저에 에러 내용을 보여준다.
+cgitb.enable()
+
+print("Content-type: text/html\n")    #\n 을 안하면 Internal Server Error 발생!
+
+print('<html>')
+print('<head>')
+print('<title>Hello Word - First CGI Program</title>')
+print('</head>')
+print('<body>')
+
+form = cgi.FieldStorage()
+
+if "email" not in form:
+    print("Error!")
+else:
+    print("Your email address is " + form["email"].value)
+
+
+print('</body>')
+print('</html>')
